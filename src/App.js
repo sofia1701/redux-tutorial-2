273febdef7cd4ab5a3863e5ee0cd41f980a8e12f
Sofia <sofia.dionisio94@gmail.com>
2020-12-09 17:03:08 +0000

    Created actions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
-  return (
-    <div className="App">
-      heloo
-    </div>
-  );
+	const counter = useSelector((state) => state.counter);
+	const isLogged = useSelector((state) => state.isLogged);
+	const dispatch = useDispatch();
+
+	return (
+		<div className="App">
+			Counter: {counter}
+			<button onClick={() => dispatch(increment())}>+</button>
+			<button onClick={() => dispatch(decrement())}>-</button>
+			<br />
+			{isLogged ? <p>Show info</p> : <p>Please, log in.</p>}
+		</div>
+	);
 }
 
 export default App;
