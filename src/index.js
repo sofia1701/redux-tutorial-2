a1aebfbd48d7d287f6a116b062f9ea60545db182
Sofia <sofia.dionisio94@gmail.com>
2020-12-09 16:36:32 +0000

    organized redux store in folders

--- src/index.js
+++ src/index.js
@@ -4,39 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
+import allReducers from './reducers/index';
 
-//Store - Globalized state
-
-//Action Increment
-const increment = () => {
-	return {
-		type: 'INCREMENT',
-	};
-};
-
-const decrement = () => {
-	return {
-		type: 'DECREMENT',
-	};
-};
-//Reducer
-const counter = (state = 0, action) => {
-	// eslint-disable-next-line default-case
-	switch (action.type) {
-		case 'INCREMENT':
-			return state + 1;
-		case 'DECREMENT':
-			return state - 1;
-	}
-};
-
-let store = createStore(counter);
-
-//Display it in the console
-store.subscribe(() => console.log(store.getState()));
-
-//Dispatch
-store.dispatch(increment());
+const store = createStore(
+  allReducers, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 ReactDOM.render(
 	<React.StrictMode>
