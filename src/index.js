2b5a00ad37b81e2fdbd00fea4d9396530cd48d22
Sofia <sofia.dionisio94@gmail.com>
2020-12-09 16:40:25 +0000

    connected redux to the app

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import allReducers from './reducers/index';
+import { Provider } from 'react-redux';
 
-const store = createStore(
-  allReducers, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<Provider store={store}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Provider>,
 	document.getElementById('root')
 );
 
