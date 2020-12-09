7be83666398a96c39f5f4ba0352e8c4a7f59a9e6
Sofia <sofia.dionisio94@gmail.com>
2020-12-09 15:54:05 +0000

    created first piece of state - counter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+//Store - Globalized state
+
+//Action Increment
+const increment = () => {
+	return {
+		type: 'INCREMENT',
+	};
+};
+
+const decrement = () => {
+	return {
+		type: 'DECREMENT',
+	};
+};
+//Reducer
+const counter = (state = 0, action) => {
+	// eslint-disable-next-line default-case
+	switch (action.type) {
+		case 'INCREMENT':
+			return state + 1;
+		case 'DECREMENT':
+			return state - 1;
+	}
+};
+
+let store = createStore(counter);
+
+//Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+//Dispatch
+store.dispatch(increment());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
 
 reportWebVitals();
